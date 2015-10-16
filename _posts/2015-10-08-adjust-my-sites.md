4c647bfbf1e0dbeee41a25696ebd1a38a4bdb2e5
loumingjie <546587684@qq.com>
2015-10-16 20:19:12 +0800

    lazyload

--- _posts/2015-10-08-adjust-my-sites.md
+++ _posts/2015-10-08-adjust-my-sites.md
@@ -11,36 +11,36 @@ tags: 博客搭建 jekyll
 命令行中输入 `jekyll serve --watch`后，进入localhost：4000端口进行监听
 打开页面如下图所示
 
-<img src="{{ site.url }}/images/loading.gif" data-rsrc="{{ site.url }}/images/post/2015-10-08/1.jpg" width="720px"/>
+<img  data-original="{{ site.url }}/images/post/2015-10-08/1.jpg" />
 
 但是在这个时候图片不能显示，看了图片的路径什么的都没有错。不知道是出于什么情况，过了一会儿再进入的时候图片。在开发者调试模式下（F12）发现这个图片的Type类型不对。本来应该是`image/jpeg`，但是这张图片的type变成了`text/html`.
 
-<img src="{{ site.url }}/images/loading.gif" data-rsrc="{{ site.url }}/images/post/2015-10-08/3.png" width="720px">
+<img  data-original="{{ site.url }}/images/post/2015-10-08/3.png" >
 
 后来才发现因为读取的地址是*{{ site.url }}/images/post/2015-10-08/3.png*，所以在本地调试的时候图片读取的应该还是github远程服务器上的图片，说以还没提交到远端的图片当然不能显示咯，然后我把图片提交到github上之后在本地调试就没有出现问题了，图片也显示正常。
 
 进入文章页面，一切正常。然后停留在post页面想进入文章归档页面的时候，出现了404 Not Found！页面。
 
-<img src="{{ site.url }}/images/loading.gif" data-rsrc="{{ site.url }}/images/post/2015-10-08/4.jpg" width="720px"/>
+<img  data-original="{{ site.url }}/images/post/2015-10-08/4.jpg" />
 
 又不知道为什么了，文章引用的布局是post，post引用的布局又是default，应该不会有问题啊，从首页就能进入文章归档，但是为什么从文章页面就不行了呢？
 
 在default页面发现了问题
-<img src="{{ site.url }}/images/loading.gif" data-rsrc="{{ site.url }}/images/post/2015-10-08/5.png" width="300px"/>
+<img  data-original="{{ site.url }}/images/post/2015-10-08/5.png" />
 
 在取地址的时候用的是相对地址，所以生成的站点里取得的地址是：
 
-<img src="{{ site.url }}/images/loading.gif" data-rsrc="{{ site.url }}/images/post/2015-10-08/7.png" width="300px"/>
+<img  data-original="{{ site.url }}/images/post/2015-10-08/7.png" />
 
 只要将`config.yml`中的nav下的url改成绝对路径即可：
 
-<img src="{{ site.url }}/images/loading.gif" data-rsrc="{{ site.url }}/images/post/2015-10-08/6.png" width="300px"/>
+<img  data-original="{{ site.url }}/images/post/2015-10-08/6.png" />
 
 ##github服务器上的调试
 
 进入<a href="https://loudou140806.github.com/myblog">https://loudou140806.github.com/myblog</a>,没有问题，一切运行正常，但是在点击进入文章时出现404页面
 
-<img src="{{ site.url }}/images/loading.gif" data-rsrc="{{ site.url }}/images/post/2015-10-08/2.jpg" width="720px"/>
+<img  data-original="{{ site.url }}/images/post/2015-10-08/2.jpg" />
 
 进入`https://loudou140806.github.com/myblog`,没有问题，一切运行正常，但是在点击进入文章时出现404页面
 
---
+++ js/common.js
@@ -0,0 +1,190 @@
+/**
+ * @desc:   公用js
+ * @author: zhanxin.lin
+ * @depend: jQuery, jQuery scrollTo
+ */
+$(document).ready(function() {
+    /**
+     *  scrollTo Top
+     */
+    var scrollTo = {
+            nodeName: "J-backTop",
+            scrollHeight: "100",
+            linkBottom: "110px",
+            linkRight: "1em",
+            _scrollTop: function() {
+            if(jQuery.scrollTo) {
+                jQuery.scrollTo(0, 800, {queue:true});
+            }
+        },
+        _scrollScreen: function() {
+            var that = this, topLink = $('#' + that.nodeName);
+
+            if(jQuery(document).scrollTop() <= that.scrollHeight) {
+                topLink.hide();
+                return true;
+            }  else {
+                topLink.fadeIn();
+            }
+        },
+        _resizeWindow: function() {
+            var that = this, topLink = $('#' + that.nodeName);
+            if($(window).width() > 1024) {
+                var leftSize = $(window).width() / 2 + 400;
+                topLink.css({
+                    'right' : '',
+                    'left': leftSize + 'px',
+                    'bottom': that.linkBottom
+                });
+            } else {
+                topLink.css({
+                    'left': '',
+                    'right' : that.linkRight,
+                    'bottom': that.linkBottom
+                });
+            }
+
+
+        },
+        run: function() {
+            var that = this, topLink = $('<a id="' + that.nodeName + '" href="#" class="toTop"><i class="fa fa-chevron-up"></i></a>');
+            topLink.appendTo($('body'));
+
+            topLink.css({
+                'display': 'none',
+                'position': 'fixed',
+                'left': '',
+                'right': that.linkRight,
+                'bottom': that.linkBottom
+            });
+
+            that._resizeWindow();
+
+            if(jQuery.scrollTo) {
+                topLink.click(function() {
+                    that._scrollTop();
+                    return false;
+                });
+            }
+            jQuery(window).resize(function() {
+                that._scrollScreen();
+                that._resizeWindow();
+            });
+            jQuery(window).scroll(function() {
+                that._scrollScreen();
+            });
+        }
+    }
+    scrollTo.run();
+
+    var EventUtil = {
+
+        addHandler: function(element, type, handler){
+            if (element.addEventListener){
+                element.addEventListener(type, handler, false);
+            } else if (element.attachEvent){
+                element.attachEvent("on" + type, handler);
+            } else {
+                element["on" + type] = handler;
+            }
+        },
+        
+        getButton: function(event){
+            if (document.implementation.hasFeature("MouseEvents", "2.0")){
+                return event.button;
+            } else {
+                switch(event.button){
+                    case 0:
+                    case 1:
+                    case 3:
+                    case 5:
+                    case 7:
+                        return 0;
+                    case 2:
+                    case 6:
+                        return 2;
+                    case 4: return 1;
+                }
+            }
+        },
+        
+        getCharCode: function(event){
+            if (typeof event.charCode == "number"){
+                return event.charCode;
+            } else {
+                return event.keyCode;
+            }
+        },
+        
+        //得到剪贴板内容
+        getClipboardText: function(event){
+            var clipboardData =  (event.clipboardData || window.clipboardData);
+            return clipboardData.getData("text");
+        },
+        
+        getEvent: function(event){
+            return event ? event : window.event;
+        },
+        
+        getRelatedTarget: function(event){
+            if (event.relatedTarget){
+                return event.relatedTarget;
+            } else if (event.toElement){
+                return event.toElement;
+            } else if (event.fromElement){
+                return event.fromElement;
+            } else {
+                return null;
+            }
+        
+        },
+        
+        getTarget: function(event){
+            return event.target || event.srcElement;
+        },
+        
+        getWheelDelta: function(event){
+            if (event.wheelDelta){
+                return (client.engine.opera && client.engine.opera < 9.5 ? -event.wheelDelta : event.wheelDelta);
+            } else {
+                return -event.detail * 40;
+            }
+        },
+        
+        preventDefault: function(event){
+            if (event.preventDefault){
+                event.preventDefault();
+            } else {
+                event.returnValue = false;
+            }
+        },
+
+        removeHandler: function(element, type, handler){
+            if (element.removeEventListener){
+                element.removeEventListener(type, handler, false);
+            } else if (element.detachEvent){
+                element.detachEvent("on" + type, handler);
+            } else {
+                element["on" + type] = null;
+            }
+        },
+        
+        //设置剪贴板内容
+        setClipboardText: function(event, value){
+            if (event.clipboardData){
+                event.clipboardData.setData("text/plain", value);//Chorme是text/plain
+            } else if (window.clipboardData){
+                window.clipboardData.setData("text", value);//IE是text
+            }
+        },
+        
+        stopPropagation: function(event){
+            if (event.stopPropagation){
+                event.stopPropagation();
+            } else {
+                event.cancelBubble = true;
+            }
+        }
+
+    };
+});
